c3256d7914870122df8bb9515feed5ed199cee85
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-04 23:49:29 -0300

    final da aula64

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,42 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		//AULA64
+		// AULA64
+
+		Locale.setDefault(Locale.US);
+		Scanner edd = new Scanner(System.in);
+
+		double xA, xB, xC, yA, yB, yC;
+
+		System.out.println("MEDIDAS DE X");
+		xA = edd.nextDouble();
+		xB = edd.nextDouble();
+		xC = edd.nextDouble();
+		System.out.println("MEDIDAS DE Y");
+		yA = edd.nextDouble();
+		yB = edd.nextDouble();
+		yC = edd.nextDouble();
+
+		double p = (xA + xB + xC) / 2;
+		double areax = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		p = (yA + yB + yC) / 2;
+		double areay = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
 		
+		System.out.printf("AREA DE X: %.4f%n",areax);
+		System.out.printf("AREA DE X: %.4f%n",areay);
+
 		
+		if (areax>areay) {
+			System.out.println("AREA X É MAIOR");
+		}else {
+			System.out.println("AREA Y É MAIOR");
+		}
+		edd.close();
 
 	}
 

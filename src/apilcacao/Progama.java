2675328251c2bc11527d92ac7de39674ec9680c6
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-09 23:01:42 -0300

    Final da aula67

--- src/apilcacao/Progama.java
+++ src/apilcacao/Progama.java
@@ -1,10 +1,29 @@
 package apilcacao;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidade.Produto;
+
 public class Progama {
 
 	public static void main(String[] args) {
 		// AULA 67
-		
+
+		Locale.setDefault(Locale.US);
+		Scanner edd = new Scanner(System.in);
+
+		Produto produto = new Produto();
+
+		System.out.println("DADOS DO PRODUTO");
+		System.out.print("PRODUTO:");
+		produto.nome = edd.nextLine();
+		System.out.print("PREÇO:");
+		produto.preco = edd.nextDouble();
+		System.out.print("QUANTIDADE:");
+		produto.quantidade = edd.nextInt();
+
+		edd.close();
 	}
 
 }
